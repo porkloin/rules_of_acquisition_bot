db683b3130502c395c2ae4453f5f96dc9324b9f9
Schuyler Schwafel <schuyler.schwafel@pitchbook.com>
2017-02-16 08:48:18 -0800

    Syntax changes

--- acquisitionbot.py
+++ acquisitionbot.py
@@ -6,15 +6,25 @@ import random
 from slackclient import SlackClient
 
 
-#bot's ID from environment var
+# bot's ID from environment var
 BOT_ID = os.environ.get("BOT_ID")
 jsonfile = 'ferengiRulesOfAcquisition.json'
-#constants
+# constants
 AT_BOT = "<@" + str(BOT_ID) + ">"
 EXAMPLE_COMMAND = "random"
 REGEX = re.compile('\d+')
-#start client
+# start client
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+# Make a list of valid commands
+validCommands = ["random"]
+
+# Load the JSON into memory
+with open(jsonfile) as f:
+    rules = json.load(f)
+
+import ipdb; ipdb.set_trace()  # XXX BREAKPOINT
+
 
 def handle_command(command, channel):
     """
@@ -23,16 +33,16 @@ def handle_command(command, channel):
         necessary
     """
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + "* command or enter a specific number."
-    if command.startswith(EXAMPLE_COMMAND):
-        with open (jsonfile) as f:
-            data = json.load(f)
-            choice = random.choice(data)
+
+    if command in validCommands:
+        with open(jsonfile) as f:
+            choice = random.choice(rules)
             response = "Rule of Acquisition number " + str(choice['Number']) + ": " + choice['Rule']
             f.close()
     if REGEX.match(command):
-        with open (jsonfile) as f:
+        with open(jsonfile) as f:
             data = json.load(f)
-            choice = None;
+            choice = None
             for i in data:
                 if i.get('Number') == int(command):
                     choice = i
@@ -43,6 +53,7 @@ def handle_command(command, channel):
             f.close()
     slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
+
 def parse_slack_output(slack_rtm_output):
     """
         Find out if someone is talking at us
@@ -51,14 +62,13 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                #return text if @ mentioned
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                                               output['channel']
+                # return text if @ mentioned
+                return output['text'].split(AT_BOT)[1].strip().lower(), output['channel']
     return None, None
 
 if __name__ == "__main__":
-    #wait a sec
-    READ_WEBSOCKET_DELAY = 1 
+    # wait a sec
+    READ_WEBSOCKET_DELAY = 1
     if slack_client.rtm_connect():
         print("Connected, ready")
         while True:
@@ -68,4 +78,3 @@ if __name__ == "__main__":
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Check your junk, token and ID, etc.")
-
